b0febfa0ed77042719422fae4056734574671960
yoav <yoavdan.m@gmail.com>
2022-10-11 21:16:33 +0300

    fitst commit

---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+
+</script>
+
+
+<template>
+    <h2>Header</h2>
+</template>
+
+
+<style lang='scss'>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Footer from '~/components/Footer.vue'
+import Navbar from '~/components/Navbar.vue'
+</script>
+
+<template>
+  <div id="app-layout">
+    <main id="app">
+      <Navbar />
+      <slot />
+      <Footer />
+    </main>
+  </div>
+</template>
